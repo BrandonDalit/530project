5232eca623bfc211d0b361a15c57adf243278bf4
Brandon Dalit <92127751+BrandonDalit@users.noreply.github.com>
2022-04-07 15:00:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+#main part of the project 
+#either boolean the read.py
+#read.py returns a string for easier comparing
+#string1 == string1 == 1 scoop of food
+#string1 != string 2 == 0 scoop of food
+#string2 == string2 == 2 scoops of food
+#then loop around 
 
