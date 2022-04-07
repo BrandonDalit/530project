6d92a1c2905f4b423884ae5fc363bc72f4d6c7f5
Brandon Dalit <92127751+BrandonDalit@users.noreply.github.com>
2022-04-07 15:20:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,37 @@
 #string2 == string2 == 2 scoops of food
 #then loop around 
 
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+GPIO.setwarnings(False)
+
+pw_1 = "onescoop"
+pw_2 = "twoscoop"
+
+x = 0
+
+reader = SimpleMFRC522()
+
+try:
+        print("tap NFC Card")
+        id, text = reader.read()
+        print(id)
+        print(text)
+
+        while x < 1 :
+                print("tap NFC Card")
+                id, text = reader.read()
+                print(id)
+                print(text)
+
+        if text == pw_1:
+                print("1 scoop")
+
+        elif text == pw_2:
+                print("2 scoop")
+
+        else:
+                print("0 scoop")
+
+finally:
+        GPIO.cleanup()
