b5a0e9b6d3223857a7a609885708c09ada080319
Brandon Dalit <92127751+BrandonDalit@users.noreply.github.com>
2022-04-20 13:59:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ flag2 = False
 #activate the servo
 pwm = GPIO.PWM(servo_control_pin, pwm_freq)
 
+#Time calculations
 def timer(text):
     global tb1, tb2, te1, te2, tn1, tn2
     now = time.time()
@@ -59,8 +60,8 @@ def timer(text):
     else:
         tn1 = now
         tn2 = now
-    #print("tn1: " + time.ctime(tn1) + " tn2: " + time.ctime(tn2) + "\ntb1: " + time.ctime(tb1) + " tb2: " + time.ctime(tb2) + "\nte1: " + time.ctime(te1) + " te2: " + ti>
         
+#Time check if time now has not passed the ending time       
 def timout():
     global tb1, te2, flag1, flag2, tn1, tn2 
     tn1 = time.time()
@@ -77,47 +78,37 @@ def timout():
 
     if tn2 >= te2:
         flag2 = True
-    #print("fg1: " + str(flag1) + ", fg2: " + str(flag2)) 
-    #print("tn1: " + time.ctime(tn1) + " tn2: " + time.ctime(tn2) + "\nte1: " + time.ctime(te1) + " te2: " + time.ctime(te2))
 
+#Checks if password is matching, and once matched dispenses food by password.
 def check(text):
     global co
 
     if text == pw_1 :
-        #print("1 scoop")
         pwm.start(forward)
         time.sleep(.3)
         pwm.ChangeDutyCycle(backward)
-        #time.sleep(3)
         co = 0
 
     if text == pw_2 :
-        #print("2 scoop")
         pwm.start(forward)
         time.sleep(.6)        
         pwm.ChangeDutyCycle(backward)
         co = 0
     
     if text != pw_1 and text != pw_2:
-        print("0 scoop")
         co = co + 1
-        #time.sleep(2)
     
     if co == 5:
        print("Cooldown...")
        co = 0
        time.sleep(30)
-    #print("5 seconds")
-    #time.sleep(5)
+    
     print("ready...\n")
 
 reader = SimpleMFRC522()
 
 try:
-        #print("tap NFC Card")
-        id, text = reader.read()
-        #print(id)
-        #print(text)
+        id, text = reader.read() #reads NFC contents and assings them to each ID and TEXT password
         pwm.start(0)
 
         while True:
@@ -126,6 +117,7 @@ try:
                 id, text = reader.read()
                 print("NFC Password scanned is: " + text)
                 
+                #checks for matching password then, timout if it has passed the end time, if so then calculates the next time, lastly dispenses food
                 if text.strip() == pw_1:
                     timout()
                     timer(pw_1)
@@ -135,7 +127,8 @@ try:
                     if tn1 >= te1  and flag1 == True:
                         print("Time started: " + time.ctime(tn1) + ", timeout will halt after " + time.ctime(tn1 + t1) + ".")
                         check(text.strip())
-                    
+                        
+                #checks for matching password then, timout if it has passed the end time, if so then calculates the next time, lastly dispenses food    
                 elif text.strip() == pw_2:
                     timout()
                     timer(pw_2)
