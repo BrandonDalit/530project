6b7f0bd5a4687632eb2b261f17f24ae4ec197d14
Brandon Dalit <92127751+BrandonDalit@users.noreply.github.com>
2022-11-13 00:10:04 -0800

    check

--- read.py
+++ read.py
@@ -7,9 +7,11 @@ reader = SimpleMFRC522()
 
 try:
         print("tap NFC Card")
-        id, text = reader.read()
+        #id, text = reader.read()
+        id, text = self.read_no_block()
         print(id)
         print(text)
+        
 finally:
         GPIO.cleanup()
 
