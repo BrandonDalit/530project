000fa419e95923b45d6df6d63048d8e6a5e77b36
Brandon Dalit <bdalit8547@sdsu.edu>
2022-11-13 14:21:14 -0800

    working now for some reason

--- read.py
+++ read.py
@@ -8,7 +8,7 @@ reader = SimpleMFRC522()
 try:
         print("tap NFC Card")
         #id, text = reader.read()
-        id, text = reader.read_no_block()
+        id, text = reader.read()
         print(id)
         print(text)
         
